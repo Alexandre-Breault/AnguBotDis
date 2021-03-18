a5fbab6ec4361e0e07fec20a1d85303600554567
Alexandre BREAULT <a.breault@iia-laval.fr>
2021-03-18 21:35:14 +0100

    Start website Front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommandComponent } from './command/command.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChartjsComponent } from './chartjs/chartjs.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Commandes', component: CommandComponent },
+  { path: 'Dashboard', component: DashboardComponent },
+  { path: 'test', component: ChartjsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CommandComponent } from './command/command.component';
+import { ChartjsComponent } from './chartjs/chartjs.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    CommandComponent,
+    ChartjsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
